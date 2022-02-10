83a030b6d0a2aae4f1bc7c929860f927577d0109
Tomas36M <tomasmuenvar36@gmail.com>
2022-02-09 20:58:10 -0500

    second-commit

--- database.js
+++ database.js
@@ -1,7 +1,13 @@
 module.exports = {
-    HOST: '192.168.64.2',
-    USER: 'tomas',
-    PASSWORD: '1234',
-    DB: 'netflix_db',
-    DIALECT: 'mysql'
+    HOST: 'ec2-54-159-244-207.compute-1.amazonaws.com',
+    USER: 'mmcefppjntuqli',
+    PASSWORD: 'bd90be89fcf4b05a31ddce6e78eaf410b6b10c5f20c46a471a629642f697b981',
+    DB: 'dbkmcmu8aqfso7',
+    DIALECT: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 }
